a5d1acc151e66b151d85be1ec1719aeb00c11f4b
rendikusuma23 <48547873+rendikusuma23@users.noreply.github.com>
2019-04-20 13:02:06 +0700

    Add files via upload

---
+++ Perkenalan/src/perkenalan/Perkenalan.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package perkenalan;
+
+/**
+ *
+ * @author G40
+ */
+public class Perkenalan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Assalamualaikum");
+        System.out.println("Perkenalkan, saya Rendi kusuma");
+        System.out.println("Panggil saja Rendi, dari kelas4C");
+    }
+}
